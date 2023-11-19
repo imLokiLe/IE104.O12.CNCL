cfddb4d12e60d4d731af400ce7ebdbf7d8b48072
ngocng202 <ngocng202@gmail.com>
2023-11-19 11:51:21 +0700

    contact page

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  // nếu người dùng không nhập gì vào trường đó, thì loại bỏ class "focus" từ phần tử cha.
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
