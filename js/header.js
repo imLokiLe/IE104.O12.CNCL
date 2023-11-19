d25fd7a4f0eef08d8a546dd6a96988cd35738df6
Loc Le <20521546@gm.uit.edu.vn>
2023-11-19 11:27:09 +0700

    modified organization

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const menuItems = document.querySelectorAll('.menu-item');
     const serviceChildLinks = document.querySelectorAll('.service-child-link');
     const serviceLink = document.querySelector('.parent-item');
--- js/link_header_footer.js
+++ js/link_header_footer.js
@@ -1,27 +1,26 @@
 function loadHTML(url, elementId) {
-  fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById(elementId).innerHTML = data;
-    });
-}
-
-function loadCSS(url) {
-  var link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href = url;
-  document.head.appendChild(link);
-}
-
-function loadJS(url) {
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-}
-
-loadHTML("./header.html", "header");
-loadHTML("./footer.html", "footer");
-loadCSS("./css/header.css");
-loadCSS("./css/footer.css");
-loadJS("./js/header.js");
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+      });
+  }
+  
+  function loadCSS(url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    document.head.appendChild(link);
+  }
+  
+  function loadJS(url){
+    var srcipt = document.createElement("script");
+    srcipt.src = url;
+    document.head.appendChild(script);
+  }
+  loadHTML("./header.html", "header");
+  loadHTML("./footer.html", "footer");
+  loadCSS("./css/header.css");
+  loadCSS("./css/footer.css");
+  loadJS("./js/header.js");
