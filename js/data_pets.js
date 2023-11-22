7e31dc32402bc48bb038a9be3106ee8128ef2ec6
TTTThanh <thanhbmt0902@gmail.com>
2023-11-22 14:03:05 +0700

    css filter, tạo data pet giả, show pet category bằng js

---
+++ js/data_pets.js
@@ -0,0 +1,64 @@
+const pets = [
+    {
+        id: 1,
+        image: '../assets/images/Pets/alaska.png',
+        name: 'Alaska Malamute trắng xám 1', 
+        price: '11,000,000',
+        unit: 'đ',
+        rating: '5',
+        featured: {
+            genus: 'dog',
+            type: 'akita',
+        }
+    },
+    {
+        id: 2,
+        image: '../assets/images/Pets/alaska.png',
+        name: 'Alaska Malamute trắng xám 2', 
+        price: '19,000,000',
+        unit: 'đ',
+        rating: '4',
+        featured: {
+            genus: 'dog',
+            type: 'akita',
+        }
+    },
+    {
+        id: 3,
+        image: '../assets/images/Pets/alaska.png',
+        name: 'Alaska Malamute trắng xám 3', 
+        price: '13,000,000',
+        unit: 'đ',
+        rating: '2',
+        featured: {
+            genus: 'dog',
+            type: 'akita',
+        }
+    },
+    {
+        id: 4,
+        image: '../assets/images/Pets/alaska.png',
+        name: 'Alaska Malamute trắng xám 4', 
+        price: '26,000,000',
+        unit: 'đ',
+        rating: '5',
+        featured: {
+            genus: 'dog',
+            type: 'akita',
+        }
+    },
+    {
+        id: 5,
+        image: '../assets/images/Pets/alaska.png',
+        name: 'Alaska Malamute trắng xám 5', 
+        price: '14,000,000',
+        unit: 'đ',
+        rating: '4',
+        featured: {
+            genus: 'dog',
+            type: 'akita',
+        }
+    },
+];
+
+export { pets };
