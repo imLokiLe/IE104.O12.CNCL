9dfee0f8009d28efa26695cd871a6268df4127eb
ngocng202 <ngocng202@gmail.com>
2023-11-26 23:52:16 +0700

    update new filter for blog

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,7 @@
-const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
+const optionMenu = document.querySelector(".select-menu1"),
+       selectBtn = optionMenu.querySelector(".select-btn1"),
+       options = optionMenu.querySelectorAll(".option1"),
+       sBtn_text = optionMenu.querySelector(".sBtn-text1");
 
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
@@ -14,6 +14,21 @@ options.forEach(option =>{
     });
 });
 
+const optionMenu2 = document.querySelector(".select-menu2"),
+       selectBtn2 = optionMenu2.querySelector(".select-btn2"),
+       options2 = optionMenu2.querySelectorAll(".option2"),
+       sBtn_text2 = optionMenu2.querySelector(".sBtn-text2");
+
+selectBtn2.addEventListener("click", () => optionMenu2.classList.toggle("active"));       
+
+options2.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text2.innerText = selectedOption;
+
+        optionMenu2.classList.remove("active");
+    });
+});
 
 
   
