c0c7e463ee435aea6434b617f9248d91e4cc06c1
Loc Le <20521546@gm.uit.edu.vn>
2023-11-28 21:14:51 +0700

    home page completely

--- js/show_video.js
+++
@@ -1,8 +0,0 @@
-function openModal() {
-    document.getElementById('videoModal').style.display = 'block';
-    document.getElementById('videoModal').style.animation = 'none';
-  }
-
-  function closeModal() {
-    document.getElementById('videoModal').style.display = 'none';
-  }
