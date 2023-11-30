a96a1dc9bb2eda4a32c8722a2df553768f9ecead
Loc Le <20521546@gm.uit.edu.vn>
2023-11-30 21:38:41 +0700

    complete exercise page

---
+++ js/exercise.js
@@ -0,0 +1,10 @@
+// scroll smooth
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+  
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth" // Để có hiệu ứng mượt mà, có thể sử dụng "auto" nếu không cần hiệu ứng
+      });
+    }
+  }
